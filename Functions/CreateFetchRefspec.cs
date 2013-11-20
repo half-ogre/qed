baeb1c4027051fde0e7a2431850b42d245bb21a4
half-ogre <nemo@half-ogre.com>
2013-11-20 14:40:09 -0800

    function to create a refspec to fetch

---
+++ Functions/CreateFetchRefspec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static string CreateFetchRefspec(string @ref)
+        {
+            if (@ref.StartsWith("refs/heads/", StringComparison.OrdinalIgnoreCase))
+            {
+                var branch = @ref.Substring(11);
+                return String.Format("+refs/heads/{0}:refs/remotes/origin/{0}", branch);
+            }
+
+            if (@ref.StartsWith("refs/pull/", StringComparison.OrdinalIgnoreCase))
+            {
+                var slashafterPrNumberIndex = @ref.IndexOf("/", 10, StringComparison.InvariantCultureIgnoreCase);
+                var prNumber = @ref.Substring(10, slashafterPrNumberIndex - 10);
+                return String.Format("+refs/pull/{0}/head:refs/remotes/origin/pr/{0}", prNumber);
+            }
+
+            throw new ArgumentException(String.Format("Unexpected type of ref: {0}.", @ref), "ref");
+        }
+    }
+}
