6df6c6483c6e8ee6ee35137be38a433e766a868a
half-ogre <nemo@half-ogre.com>
2013-11-24 02:39:47 -0800

    get a repo's next build

---
+++ RavenQueries/GetNextBuild.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static Build GetNextBuild(
+            string repositoryOwner, 
+            string repositoryName)
+        {
+            using (var ravenSession = OpenRavenSession())
+            {
+                return ravenSession.Query<Build>()
+                    .Where(build => build.RepositoryOwner == repositoryOwner &&
+                                    build.RepositoryName == repositoryName && 
+                                    build.Started == null)
+                    .OrderBy(build => build.Queued)
+                    .FirstOrDefault();
+            }
+        }
+    }
+}
