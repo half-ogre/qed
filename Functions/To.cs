d53e0354c8497fdbe1d72764b1b3abee241bcbcc
half-ogre <nemo@half-ogre.com>
2013-11-23 21:02:30 -0800

    add To function to transform objects

---
+++ Functions/To.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static object To<T>(
+            this T @object,
+            Func<T, object> to)
+            where T : class
+        {
+            if (@object == null)
+                return null;
+
+            return to(@object);
+        }
+    }
+}
