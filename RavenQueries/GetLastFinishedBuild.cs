5f07f58918ede2dc2396e8dda9c95c58b5e60c06
half-ogre <nemo@half-ogre.com>
2013-11-23 22:09:58 -0800

    query to get last finished build

---
+++ RavenQueries/GetLastFinishedBuild.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static Build GetLastFinishedBuild(
+            string repositoryOwner, 
+            string repositoryName)
+        {
+            using (var ravenSession = OpenRavenSession())
+            {
+                return ravenSession.Query<Build>()
+                    .Where(build => build.RepositoryOwner == repositoryOwner &&
+                                    build.RepositoryName == repositoryName && 
+                                    build.Finished != null)
+                    .OrderByDescending(build => build.Started)
+                    .FirstOrDefault();
+            }
+        }
+    }
+}
