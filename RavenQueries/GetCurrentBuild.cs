9b9f67601994ee73456e5e18b72c02ba8b17e5f0
half-ogre <nemo@half-ogre.com>
2013-11-24 02:39:23 -0800

    get a repo's current build

---
+++ RavenQueries/GetCurrentBuild.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static Build GetCurrentBuild(
+            string repositoryOwner,
+            string repositoryName)
+        {
+            using (var ravenSession = OpenRavenSession())
+            {
+                return ravenSession.Query<Build>()
+                    .Where(build => build.RepositoryOwner == repositoryOwner &&
+                                    build.RepositoryName == repositoryName &&
+                                    build.Started != null &&
+                                    build.Finished == null)
+                    .OrderByDescending(build => build.Started)
+                    .FirstOrDefault();
+            }
+        }
+    }
+}
