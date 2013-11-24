8af03768af62791d31fa6d4866bda2c0d71fa2a8
half-ogre <nemo@half-ogre.com>
2013-11-23 21:52:49 -0800

    function to build a description of a build

---
+++ Functions/GetBuildDescription.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static string GetBuildDescription(Build build)
+        {
+            return GetBuildDescription(build, DateTimeOffset.UtcNow);
+        }
+
+        internal static string GetBuildDescription(Build build, DateTimeOffset now)
+        {
+            if (build == null) throw new ArgumentNullException("build");
+
+            if (!build.Started.HasValue)
+            {
+                if (!build.Queued.HasValue)
+                    return String.Format("Build #{0} queued.", build.Id);
+
+                return String.Format("Build #{0} queued {1} seconds ago.", build.Id, build.Queued.Value.Since(now));
+            }
+
+            if (!build.Finished.HasValue)
+                return String.Format("Build #{0} started {1} seconds ago.", build.Id, build.Started.Value.Since(now));
+
+            if (!build.Succeeded.HasValue)
+                return String.Format("Build #{0} finished in {1} seconds.", build.Id, build.Started.Value.Until(build.Finished.Value));
+
+            if (build.Succeeded.Value)
+                return String.Format("Build #{0} succeeded in {1} seconds.", build.Id, build.Started.Value.Until(build.Finished.Value));
+
+            return String.Format("Build #{0} failed in {1} seconds.", build.Id, build.Started.Value.Until(build.Finished.Value));
+        }
+    }
+}
---
+++ UnitTests/Functions/GetBuildDescriptionUnitTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Xunit;
+using fn = qed.Functions;
+
+#if DEBUG
+namespace qed.UnitTests
+{
+    public class the_GetBuildDescription_function
+    {
+        [Fact]
+        public void returns_description_for_a_build_not_started_with_no_queued_timestamp()
+        {
+            var build = new Build
+            {
+                Finished = null,
+                Id = new Guid(),
+                Queued = null,
+                Started = null,
+                Succeeded = null
+            };
+
+            var actual = fn.GetBuildDescription(build);
+
+            Assert.Equal(String.Format("Build #{0} queued.", build.Id), actual);
+        }
+
+        [Fact]
+        public void returns_description_for_a_build_not_started_with_a_queued_timestamp()
+        {
+            var queued = DateTimeOffset.UtcNow;
+            var build = new Build
+            {
+                Finished = null,
+                Id = new Guid(),
+                Queued = queued,
+                Started = null,
+                Succeeded = null
+            };
+
+            var actual = fn.GetBuildDescription(build);
+
+            Assert.Equal(String.Format("Build #{0} queued {1} seconds ago.", build.Id, queued.Since()), actual);
+        }
+
+        [Fact]
+        public void returns_description_for_a_build_started_but_not_finished()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var build = new Build
+            {
+                Finished = null,
+                Id = new Guid(),
+                Queued = DateTimeOffset.UtcNow,
+                Started = now.AddSeconds(-42),
+                Succeeded = null
+            };
+
+            var actual = fn.GetBuildDescription(build, now);
+
+            Assert.Equal(String.Format("Build #{0} started {1} seconds ago.", build.Id, 42), actual);
+        }
+
+        [Fact]
+        public void returns_description_for_a_finished_build_with_no_succeeded_bit()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var build = new Build
+            {
+                Finished = now.AddSeconds(-60),
+                Id = new Guid(),
+                Queued = DateTimeOffset.UtcNow,
+                Started = now.AddSeconds(-102),
+                Succeeded = null
+            };
+
+            var actual = fn.GetBuildDescription(build, now);
+
+            Assert.Equal(String.Format("Build #{0} finished in {1} seconds.", build.Id, 42), actual);
+        }
+
+        [Fact]
+        public void returns_description_for_a_successful_finished()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var build = new Build
+            {
+                Finished = now.AddSeconds(-60),
+                Id = new Guid(),
+                Queued = DateTimeOffset.UtcNow,
+                Started = now.AddSeconds(-102),
+                Succeeded = true
+            };
+
+            var actual = fn.GetBuildDescription(build, now);
+
+            Assert.Equal(String.Format("Build #{0} succeeded in {1} seconds.", build.Id, 42), actual);
+        }
+
+        [Fact]
+        public void returns_description_for_a_failed_finished()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var build = new Build
+            {
+                Finished = now.AddSeconds(-60),
+                Id = new Guid(),
+                Queued = DateTimeOffset.UtcNow,
+                Started = now.AddSeconds(-102),
+                Succeeded = false
+            };
+
+            var actual = fn.GetBuildDescription(build, now);
+
+            Assert.Equal(String.Format("Build #{0} failed in {1} seconds.", build.Id, 42), actual);
+        }
+    }
+}
+#endif
