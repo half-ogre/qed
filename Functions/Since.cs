3549580df95ffef7180d9e4e9eb217075aadb385
half-ogre <nemo@half-ogre.com>
2013-11-23 21:49:12 -0800

    Since function to get seconds from date to now

---
+++ Functions/Since.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static double Since(this DateTimeOffset timestamp)
+        {
+            return Since(timestamp, DateTimeOffset.UtcNow);
+        }
+
+        internal static double Since(this DateTimeOffset timestamp, DateTimeOffset now)
+        {
+            return Math.Round((now - timestamp).TotalSeconds);
+        }
+    }
+}
---
+++ UnitTests/Functions/SinceUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using fn = qed.Functions;
+
+#if DEBUG
+namespace qed.UnitTests
+{
+    public class the_Since_function
+    {
+        [Fact]
+        public void returns_the_seconds_since_now()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var actual = now.AddSeconds(-42).Since(now);
+
+            Assert.Equal(42, actual);
+        }
+    }
+}
+#endif
