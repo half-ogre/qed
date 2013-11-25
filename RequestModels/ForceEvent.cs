abdeb681f9a6dedd88eeeece85cf110d2409ee81
half-ogre <nemo@half-ogre.com>
2013-11-25 14:31:52 -0800

    don't require a SHA for forced builds

---
+++ RequestModels/ForceEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace qed
+{
+    public class ForceEvent
+    {
+        public string Revision { get; set; }
+        public string Ref { get; set; }
+        public Repository Repository { get; set; }
+    }
+}
