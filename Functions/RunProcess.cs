c72a7a1113b1ce39996fc10ae51061b276809da7
half-ogre <nemo@half-ogre.com>
2013-11-30 10:01:25 -0800

    don't be silly

--- Functions/RunProcess.cs
+++ Functions/RunProcess.cs
@@ -23,46 +23,39 @@ namespace qed
                 return timeout - unchecked((int)stopwatch.ElapsedMilliseconds);
             });
 
-            try
+            log(String.Format(
+                "Running {0} {1} in {2}",
+                process.StartInfo.FileName,
+                process.StartInfo.Arguments,
+                process.StartInfo.WorkingDirectory));
+
+            process.Start();
+
+            var outputTask = new Task(() =>
             {
-                log(String.Format(
-                    "Running {0} {1} in {2}",
-                    process.StartInfo.FileName,
-                    process.StartInfo.Arguments,
-                    process.StartInfo.WorkingDirectory));
+                while (!process.StandardOutput.EndOfStream)
+                    log(process.StandardOutput.ReadLine());
+            });
+            outputTask.Start();
 
-                process.Start();
-
-                var outputTask = new Task(() =>
-                {
-                    while (!process.StandardOutput.EndOfStream)
-                        log(process.StandardOutput.ReadLine());
-                });
-                outputTask.Start();
-
-                var errorTask = new Task(() =>
-                {
-                    while (!process.StandardError.EndOfStream)
-                        log(process.StandardError.ReadLine());
-                });
-                errorTask.Start();
-
-                if (!Task.WaitAll(new[] {outputTask, errorTask}, getRemainingTimeout()) ||
-                    !process.WaitForExit(getRemainingTimeout()))
-                {
-                    log(String.Format("ERROR: The process ({0}) exceeded the timeout ({1}s); terminating now.", process.Id, timeout / 1000));
-                    process.Kill();
-                    process.Dispose();
-                    return int.MaxValue;
-                }
-
-                var exitCode = process.ExitCode;
-                return exitCode;
-            }
-            finally
+            var errorTask = new Task(() =>
             {
-                stopwatch.Stop();
+                while (!process.StandardError.EndOfStream)
+                    log(process.StandardError.ReadLine());
+            });
+            errorTask.Start();
+
+            if (!Task.WaitAll(new[] {outputTask, errorTask}, getRemainingTimeout()) ||
+                !process.WaitForExit(getRemainingTimeout()))
+            {
+                log(String.Format("ERROR: The process ({0}) exceeded the timeout ({1}s); terminating now.", process.Id, timeout / 1000));
+                process.Kill();
+                process.Dispose();
+                return int.MaxValue;
             }
+
+            var exitCode = process.ExitCode;
+            return exitCode;
         }
     }
 }
