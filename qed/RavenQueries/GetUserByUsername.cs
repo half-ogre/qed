42f58f15657d74725da524315d9e1e626eb54a8e
half-ogre <nemo@half-ogre.com>
2013-12-05 15:57:43 -0800

    query to get user by username

---
+++ qed/RavenQueries/GetUserByUsername.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static User GetUserByUsername(string username)
+        {
+            using (var ravenSession = OpenRavenSession())
+            {
+                return ravenSession.Query<User>()
+                    .FirstOrDefault(user => user.Username == username);
+            }
+        }
+    }
+}
