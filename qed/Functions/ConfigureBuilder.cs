7c707f6248ce0a8eda3e47d4392b51ed340f1c68
half-ogre <nemo@half-ogre.com>
2013-12-05 16:03:06 -0800

    handler to get the sign-up form

--- qed/Functions/ConfigureBuilder.cs
+++ qed/Functions/ConfigureBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using Owin;
+using System.Security.Principal;
+using OwinExtensions;
 
 namespace qed
 {
@@ -6,6 +8,18 @@ namespace qed
     {
         public static void ConfigureBuilder(IAppBuilder builder)
         {
+            var forbidIfSignedIn = new MiddlewareFunc(next => env =>
+            {
+                var principal = env.Get<IPrincipal>("server.User");
+                if (principal != null && principal.Identity.IsAuthenticated)
+                {
+                    env.SetStatusCode(403);
+                    return env.GetCompleted();
+                }
+
+                return next(env);
+            });
+
             builder.Use(ContentType.Create());
 
             builder.Use(Mustache.Create(
@@ -17,6 +31,7 @@ namespace qed
                 dispatcher.Get("/", Handlers.GetHome);
                 dispatcher.Post("/events/push", Handlers.PostPushEvent);
                 dispatcher.Post("/events/force", Handlers.PostForceEvent);
+                dispatcher.Get("/forms/sign-up", forbidIfSignedIn, Handlers.GetSignUpForm);
                 dispatcher.Get("/{owner}/{name}", Handlers.GetBuilds);
                 dispatcher.Get("/{owner}/{name}/builds/{id}", Handlers.GetBuild);
                 dispatcher.Post("/{owner}/{name}/builds", Handlers.PostBuild);
---
+++ qed/RequestHandlers/GetSignUpForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using fn = qed.Functions;
+
+namespace qed
+{
+    public static partial class Handlers
+    {
+        public static Task GetSignUpForm(
+            IDictionary<string, object> environment,
+            Func<IDictionary<string, object>, Task> next)
+        {
+            return environment.Render("sign-up-form", null);
+        }
+    }
+}
