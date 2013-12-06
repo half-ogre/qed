29399da8981eb6bf473d042525fbc1ac1e555687
half-ogre <nemo@half-ogre.com>
2013-12-06 10:13:35 -0800

    function to get timed-out builds

---
+++ qed/RavenQueries/GetTimedOutBuilds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Raven.Client.Linq;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static IList<Build> GetTimedOutBuilds()
+        {
+            using (var ravenSession = OpenRavenSession())
+            {
+                return ravenSession.Query<Build>()
+                    .Where(build =>
+                        build.Started != null &&
+                        build.Finished == null &&
+                        build.Started < DateTimeOffset.UtcNow.AddMinutes(-5))
+                    .ToList();
+            }
+        }
+    }
+}
