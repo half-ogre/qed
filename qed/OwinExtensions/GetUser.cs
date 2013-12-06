53593a0b4cccfffaf2f8a2e180c289c516ba356a
half-ogre <nemo@half-ogre.com>
2013-12-05 20:31:17 -0800

    OWIN extension to get the user

---
+++ qed/OwinExtensions/GetUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Owin;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static User GetUser(this IDictionary<string, object> environment)
+        {
+            var owinContext = new OwinContext(environment);
+
+            var principal = owinContext.Request.User;
+
+            if (principal == null || String.IsNullOrEmpty(principal.Identity.Name))
+                return null;
+
+            return GetUserByUsername(principal.Identity.Name);
+        }
+    }
+}
