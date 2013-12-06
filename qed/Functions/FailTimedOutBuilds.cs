053d27869504fef349f7eb167487b3e38dd0847a
half-ogre <nemo@half-ogre.com>
2013-12-06 10:14:07 -0800

    set timed-out builds to failed

---
+++ qed/Functions/FailTimedOutBuilds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static void FailTimedOutBuilds(Action<string> logConsoleMessage)
+        {
+            var timedOutbuilds = GetTimedOutBuilds();
+
+            foreach (var timedOutBuild in timedOutbuilds)
+            {
+                logConsoleMessage(String.Format("Build #{0} timed out; setting to failed.", timedOutBuild.Id));
+                AppendBuildOutput(timedOutBuild, "ERROR: The build exceeded the timeout; setting to failed.");
+                SetBuildFinished(timedOutBuild, false, DateTimeOffset.UtcNow);
+            }
+        }
+    }
+}
