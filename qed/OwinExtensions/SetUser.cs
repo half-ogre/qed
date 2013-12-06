f28348aaff730a4f19c2fee628c2e382f079ac20
half-ogre <nemo@half-ogre.com>
2013-12-05 20:30:41 -0800

    OWIN extension to set the user

---
+++ qed/OwinExtensions/SetUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.Owin;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static void SetUser(
+            this IDictionary<string, object> environment,
+            User user)
+        {
+            var owinContext = new OwinContext(environment);
+
+            var identity = new GenericIdentity(user.Username, "Session");
+
+            owinContext.Authentication.SignIn(new[] { new ClaimsIdentity(identity) });
+        }
+    }
+}
