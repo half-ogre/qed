e83866ade50c198fd555494bb7a4dc7a1fd45424
half-ogre <nemo@half-ogre.com>
2013-12-05 20:32:00 -0800

    OWIN extension to remove the user

---
+++ qed/OwinExtensions/RemoveUser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.Owin;
+
+namespace qed
+{
+    public static partial class Functions
+    {
+        public static void RemoveUser(this IDictionary<string, object> environment)
+        {
+            var owinContext = new OwinContext(environment);
+
+            owinContext.Authentication.SignOut(new [] { "Session" });
+        }
+    }
+}
