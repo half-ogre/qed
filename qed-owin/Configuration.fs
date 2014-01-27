db4306b8bf4c89b2eb9d0dcaf9815982e671dc06
Drew Miller <nemo@half-ogre.com>
2014-01-27 00:57:19 -0800

    start with a dummy home page

---
+++ qed-owin/Configuration.fs
@@ -0,0 +1,9 @@
+﻿module Configuration
+
+open Owin
+open Microsoft.Owin
+
+let configureQed (appBuilder: IAppBuilder) =
+    appBuilder.Run(fun context -> 
+        context.Response.StatusCode <- 200
+        context.Response.WriteAsync "QED")
