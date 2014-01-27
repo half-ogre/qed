226218cf46f0646a714531862d68c99c294516cc
Drew Miller <nemo@half-ogre.com>
2014-01-27 00:58:05 -0800

    configure OWIN to use QED

---
+++ qed-aspnet/Startup.fs
@@ -0,0 +1,13 @@
+﻿namespace qed.AspNet
+
+open Configuration
+open Microsoft.Owin
+open Owin
+
+type OwinConfigurator() = 
+    member x.Configuration (appBuilder: IAppBuilder) =
+        configureQed appBuilder
+
+module Startup =
+    [<assembly: OwinStartup(typeof<OwinConfigurator>)>]
+    do()
