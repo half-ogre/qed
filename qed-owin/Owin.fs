0912e598de57871f473e78fda567588d483291f3
Drew Miller <nemo@half-ogre.com>
2014-01-30 14:33:28 -0800

    a module for fundamental OWIN stuff

---
+++ qed-owin/Owin.fs
@@ -0,0 +1,13 @@
+﻿module Owin
+
+open System
+open System.Collections.Generic
+open System.Threading.Tasks
+
+type OwinEnvironment = IDictionary<string, Object>
+
+type OwinApplication = Func<OwinEnvironment, Task>
+
+type OwinMiddleware = Func<OwinApplication, OwinApplication>
+
+
